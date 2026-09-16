18d182744793239d6efd1d5164db4c3d493b16e1
Lochana Kavinda <lochanabandara36@gmail.com>
2026-09-16 14:11:31 +0530

    Initial Web project upload

---
+++ framerusercontent.com/modules/g713NjwYuj7H819nSeEn/Q5czXFfs7ajLJbp3yex6/Facebook.js
@@ -0,0 +1,28 @@
+const e = (h) => {
+    return h;
+};
+let a;
+var o = (h) => {
+    return (a || (a = e(h.createElement("path", {
+        d: "M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"
+    }), "Facebook")), a);
+};
+const __FramerMetadata__ = {
+    exports: {
+        default: {
+            type: "reactComponent",
+            slots: [],
+            annotations: {
+                framerContractVersion: "1"
+            }
+        },
+        __FramerMetadata__: {
+            type: "variable"
+        }
+    }
+};
+export {
+    __FramerMetadata__,
+    o as
+    default
+};
---
+++ framerusercontent.com/modules/vIR61hwTfbWMd7pIrXPi/c7ePVo5ujOb1rskSDnkP/Close.js
@@ -0,0 +1,28 @@
+const o = (e) => {
+    return e;
+};
+let t;
+var n = (e) => {
+    return (t || (t = o(e.createElement("path", {
+        d: "M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+    }), "Close")), t);
+};
+const __FramerMetadata__ = {
+    exports: {
+        default: {
+            type: "reactComponent",
+            slots: [],
+            annotations: {
+                framerContractVersion: "1"
+            }
+        },
+        __FramerMetadata__: {
+            type: "variable"
+        }
+    }
+};
+export {
+    __FramerMetadata__,
+    n as
+    default
+};
